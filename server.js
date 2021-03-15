39f5ca389efd2d58e5900897e21df76ed1e2fb64
bspacek <74202923+bspacek@users.noreply.github.com>
2021-03-15 14:14:08 -0600

    Mongo Put Test

--- server.js
+++ server.js
@@ -131,8 +131,8 @@ router.route('/Movie')
         Movie.findOne({title: req.body.title}, function(err, result) {
             if (err) { throw err;
             } else {
-                Movie.findByIdAndUpdate(result._id, {title:req.body.title, year: req.body.year, genre:req.body.genre })
-                res.json({success:true, query: result}
+                Movie.findByIdAndUpdate(result._id, {title:req.body.title, year: req.body.year, genre:req.body.genre }, null , function (err, update))
+                res.json({success:true, msg: req.body.title + " has been updated."}
                 )}
         })
     });
